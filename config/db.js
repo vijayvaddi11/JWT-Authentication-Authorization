7b5c8f305e9f07ec60e43b58abcac7a11d52f848
Vijay Vaddi <141708089+vijayvaddi11@users.noreply.github.com>
2026-02-06 22:25:56 +0530

    Update MongoDB connection to use environment variable

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const connectDB =async()=>{
      try{
-          await mongoose.connect('mongodb://127.0.0.1:27017/login')
+          await mongoose.connect(`${process.env.MONGODB_URI}`)
           console.log('database Connected Sucess!')
      }catch(err){
           console.log('failed connecting database',err)
